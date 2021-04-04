8e006cf6e3d32addc87de07b8387e7a9383cfd6d
wangmiaozero <1061214467@qq.com>
2021-04-04 12:13:35 +0800

    net

---
+++ SignalR-samples/ChatSample/ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task Sendmsg(string name, string message,string type)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message,type);
+        }
+    }
+}
